fed778b9c7417d7f78dd03edeb58ec392307657d
Tang Quoc Van <quocvan1311@gmail.com>
2026-04-12 22:13:03 +0700

    Update trailer

--- ac-mong-10.js
+++ ac-mong-10.js
@@ -526,3 +526,37 @@ window.BOSS_TABLE_DATA = [
     "__bossColor": "#ff0000"
   }
 ];
+
+/* --- Trailer PB 10 (file .mp4 cục bộ): khớp tên file trong thư mục `trailer pb 10/` --- */
+(function () {
+  var BASE = "trailer pb 10/";
+  window.AC_MONG_TRAILER_BASE = BASE;
+  window.AC_MONG_TRAILER_FALLBACK_BASENAME =
+    "pb-33 giai xu doan vong nhai trailer.mp4";
+
+  function T(filename) {
+    return BASE + filename;
+  }
+
+  window.AC_MONG_BOSS_VIDEOS = Object.assign({}, window.AC_MONG_BOSS_VIDEOS || {}, {
+    "nhiem-no-233": [T("233 quy nguu dao boss 3 trailer.mp4")],
+    "quy-nguu-234": [T("234 quy nguu dao boss 4 trailer.mp4")],
+    "cuu-u-yeu-thu-131": [T("131 u kiep boss 1 trailer.mp4")],
+    "hac-thuy-huyen-xa-132": [T("132 u kiep boss 2 trailer.mp4")],
+    "van-doc-tam-lao-231": [T("231 quy nguu dao boss 1 trailer.mp4")],
+    "tieu-hanh-van-213": [T("pb-21 co long lang trailer.mp4")],
+    "tinh-quy-su-232": [T("232 quy nguu dao boss 2 trailter.mp4")],
+    "hoanh-hanh-ba-dao-cong-211": [T("pb-21 co long lang trailer.mp4")],
+    "du-lan-mac-xa-p2-222": [T("222 oan trung thien dia boss 2 trailer.mp4")],
+    "phuong-do-321": [
+      T("321 suong phong vo cau hai boss 1 trailer.mp4"),
+      T("pb-32 suong phong vo cau hai trailer.mp4"),
+    ],
+    "so-huyen-phuong-do-322": [
+      T("322 suong phong vo cau hai boss 2 trailer.mp4"),
+      T("pb-32 suong phong vo cau hai trailer.mp4"),
+    ],
+    "nam-tuong-quan-212": [T("pb-21 co long lang trailer.mp4")],
+    "du-lan-mac-xa-p1-221": [T("221 oan trung thien dia boss 1 trailer.mp4")],
+  });
+})();
